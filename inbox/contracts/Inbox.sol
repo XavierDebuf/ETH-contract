0afb2dd79492b17f9f27412be44e1035cc604ad0
XavierDebuf <x.debuf@gmail.com>
2023-07-06 08:53:31 +0200

    test locally with ganache

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,4 +1,14 @@
-pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
+pragma solidity >=0.4.17 <1.0.0;
 
-// contract code will go here
+contract Inbox {
+    string public message;
+    
+    function Inbox (string initalMessage) public {
+        message = initalMessage;
+    }
+    
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+   
+}
--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,6 +1,41 @@
 const ganache = require('ganache');
 const { Web3 } = require('web3');
+const assert  = require('assert');
+const web3 = new Web3(ganache.provider());
+const {interface, bytecode } = require('../compile');
 
-// updated ganache and web3 imports added for convenience
+let accounts;
+let inbox;
 
-// contract test code will go here
+beforeEach(async ()=>{
+   //get a list of all account
+    accounts = await web3.eth.getAccounts();
+   //use one of these account to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments:['it works!'] })
+    .send({ from: accounts[2], gas: '1000000' });
+});
+describe ('Inbox',()=>{
+    it('deploys a contract', ()=>{
+        assert.ok(inbox.options.address);
+    });
+   it('get a initial value', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message,'it works!');
+    });
+    it('can update value', async () => {
+        const messageModified = await inbox.methods.setMessage('message changed').send({
+            from: accounts[2]
+        });
+        const message = await inbox.methods.message().call();
+        assert.equal(message,'message changed');
+    })
+});
+/*describe ('',()=>{
+    it('', ()=>{
+
+    });
+    it('', ()=>{
+        
+    });
+});*/

b0ebb0dbc7dade7aebc66c3d2f67c44c2577524b
XavierDebuf <x.debuf@gmail.com>
2023-07-06 08:58:22 +0200

    prepare to deploy on testnet via Infura API

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,4 +1,9 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
+const {inteface, bytecode} = require('./compile');
 
-
+const provider = new HDWalletProvider(
+    'sustain clerk comfort buddy milk balcony desert knock smile deal mind eternal',
+    'https://sepolia.infura.io/v3/b5c4ddb991c54ccf82d3e43d2c318502'
+);
+const web3 = new Web3(provider);
